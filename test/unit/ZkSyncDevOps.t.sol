2dfca25174d9593a974c4e529304a2f5ed3a7244
Memo6s <144749446+Memo6s@users.noreply.github.com>
2025-07-08 22:32:25 +0000

    Update ZkSyncDevOps.t.sol

--- test/unit/ZkSyncDevOps.t.sol
+++ test/unit/ZkSyncDevOps.t.sol
@@ -7,7 +7,7 @@ import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol"
 import {FoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.sol";
 
 contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
-    // Remove the `skipZkSync`, then run `forge test --mt testZkSyncChainFails --zksync` and this will fail!
+   
     function testZkSyncChainFails() public skipZkSync {
         address ripemd = address(uint160(3));
 
@@ -19,12 +19,5 @@ contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
         assert(success);
     }
 
-    // You'll need `ffi=true` in your foundry.toml to run this test
-    // // Remove the `onlyVanillaFoundry`, then run `foundryup-zksync` and then
-    // // `forge test --mt testZkSyncFoundryFails --zksync`
-    // // and this will fail!
-    //function testZkSyncFoundryFails() public onlyVanillaFoundry {
-    //   bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
-    //  assert(exists);
-    // }
+    
 }
